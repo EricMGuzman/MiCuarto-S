2a614578070005653273bed81995c3fda64e5fde
Eric Guzmán <ericmguzmanc@gmail.com>
2015-03-23 22:38:53 -0400

    Integración Login.

--- MiCuarto S/Controllers/HomeController.cs
+++ MiCuarto S/Controllers/HomeController.cs
@@ -8,6 +8,7 @@ namespace MiCuarto_S.Controllers
 {
     public class HomeController : Controller
     {
+        [Authorize]
         public ActionResult Index()
         {
             return View();
@@ -24,6 +25,27 @@ namespace MiCuarto_S.Controllers
         {
             ViewBag.Message = "Your contact page.";
 
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Transacciones()
+        {
+            ViewBag.Message = "Pagina de transacciones.";
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Presupuesto()
+        {
+            ViewBag.Message = "Pagina de Presupuesto.";
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Opciones()
+        {
+            ViewBag.Message = "Pagina de Opciones.";
             return View();
         }
     }
---
+++ MiCuarto S/Controllers/TransactionController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MiCuarto_S.Controllers
+{
+    public class TransactionController : Controller
+    {
+        //
+        // GET: /Transaction/
+        [Authorize]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+	}
+}
